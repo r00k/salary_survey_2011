62641dc950a95f02c57647f34bced6b313f20c23
Ben Orenstein <ben.orenstein@gmail.com>
2011-03-29 16:29:27 -0400

    Do more stuff.

--- lib/salary_averager.rb
+++ lib/salary_averager.rb
@@ -8,11 +8,20 @@ class SalaryAverager
     @data.shift
   end
 
-  def average_salary
-    salaries.inject(0) { |acc, s| acc += s } / salaries.size
+  def freelancer_salaries
+    @data.select { |entry| entry[4] == "Yes" }.map { |entry| entry[1] }.map(&:to_f)
   end
 
-  def salaries
-    @data.map { |entry| entry[1] }.map(&:to_f)
+  def non_freelancer_salaries
+    @data.select { |entry| entry[4] == "No" }.map { |entry| entry[1] }.map(&:to_f)
   end
+
+  def manager_salaries
+    @data.select { |entry| entry[3] == "Yes" }.map { |entry| entry[1] }.map(&:to_f)
+  end
+
+  def non_manager_salaries
+    @data.select { |entry| entry[3] == "No" }.map { |entry| entry[1] }.map(&:to_f)
+  end
+
 end
--- spec/salary_averager_spec.rb
+++ spec/salary_averager_spec.rb
@@ -7,15 +7,27 @@ describe SalaryAverager do
     @averager = SalaryAverager.new("spec/fake_data.csv") 
   end
 
-  describe "#average_salary" do
-    it "returns the average salary for all entries" do
-      @averager.average_salary.should == 110000.0
+  describe "#freelancer_salaries" do
+    it "returns all salaries for people who are freelancers" do
+      @averager.freelancer_salaries.should == [110000.0, 120000.0]
     end
   end
 
-  describe "salaries" do
-    it "returns an array of salary floats" do
-      @averager.salaries.should == [100000.0, 120000.0]
+  describe "#non_freelancer_salaries" do
+    it "returns all salaries for people who are not freelancers" do
+      @averager.non_freelancer_salaries.should == [100000.0, 130000.0]
+    end
+  end
+
+  describe "#manager_salaries" do
+    it "returns all salaries for people who manage other devs" do
+      @averager.manager_salaries.should == [100000.0, 120000.0, 130000.0]
+    end
+  end
+
+  describe "#non_manager_salaries" do
+    it "returns all salaries for people who do not manage other devs" do
+      @averager.non_manager_salaries.should == [110000.0]
     end
   end
 end
